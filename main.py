f456067cd384bf6eba0790a9d822a419fd8eb66c
Philipnah <Philip@han.dk>
2020-09-13 22:50:19 +0200

    Added main stuff for bubble, needs implementing

--- main.py
+++ main.py
@@ -1,23 +1,74 @@
 # This file will be used to select which algorithm you want to run
 
-algorithms = [
-	"bubble sort",
-	"bucket"
-]
 
-i = 1
-for items in algorithms:
-	print(str(i) + ". " + items)
-	i += 1
+def start():
+	algorithms = [
+		"bubble sort",
+		"bucket"
+	]
+
+	i = 1
+	for items in algorithms:
+		print(str(i) + ". " + items)
+		i += 1
 
 
-choice = input("\nSelect which algorithm you want to run: ")
+	choice = input("\nSelect which algorithm you want to run: ")
 
-if choice in algorithms:
-	n = 0
-	while(n <= len(algorithms) - 1):
-		if choice == algorithms[n]:
-			print("You have selected " + algorithms[n])
-		n += 1
-else:
-	print("Your choice is not available.")
+	if choice in algorithms:
+		n = 0
+		while(n <= len(algorithms) - 1):
+			if choice == algorithms[n]:
+				print("You have selected " + algorithms[n] + "\n")
+				
+				# Try to make these following if-statements shorter, perhaps by using another method.
+				if n == 0:
+					print("0")
+					Algorithms.BubbleSort(SortThis)
+				elif n == 1:
+					Algorithms.Bucket(SortThis)
+				else:
+					print("Error, could not find your function to run.")
+
+			n += 1
+	else:
+		print("Your choice is not available.")
+
+class Algorithms:
+	#def swap(self, a, b):
+	#	c = a
+
+
+	def BubbleSort(self):
+		i = 0
+		j = i + 1
+		
+		n = 0
+		while n < len(SortThis):
+			while i < len(SortThis) - n:
+
+				a = SortThis[i]
+				b = SortThis[j]
+
+				if a > b:
+					c = a
+					a = b
+					b = c
+					print(SortThis)
+
+					#swap(a, b)
+				else:
+					pass
+
+				i += 1
+			n += 1
+
+
+
+	def Bucket(self):
+		pass
+
+SortThis = [1, 9, 3, 5, 2, 7, 6, 4, 8]
+print("This will sort the default list.\nEdit the code to select another list.\n")
+
+start()
