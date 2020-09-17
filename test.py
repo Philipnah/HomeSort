6487f17f11612d91c7dd8068baf358ab50bd1cbc
Philipnah <Philip@han.dk>
2020-09-17 09:39:53 +0200

    Added timer, new list choice & reduced print.

--- test.py
+++ test.py
@@ -1,28 +1,11 @@
-sortThis = [1, 9, 3, 5, 2, 7, 6, 4, 8]
-print("This will sort the default list.\nEdit the code to select another list.\n")
+from random import randint
 
-def BubbleSort():
-		
-	n = 0
-	while n < len(sortThis) - 1:
-		i = 0
-		while i < len(sortThis) - n - 1:
-			j = i + 1
+list1 = []
 
-			a = sortThis[i]
-			b = sortThis[j]
+i = 0
 
-			if a > b:
-				sortThis[i] = b
-				sortThis[j] = a
-				print("swapped " + str(i) + " & " + str(j))
-			else:
-				pass
+while i < 1000:
+	list1.append(randint(1, 1000))
+	i += 1
 
-			i += 1
-		n += 1
-	print(sortThis)
-		
-
-
-BubbleSort()
+print(list1)
